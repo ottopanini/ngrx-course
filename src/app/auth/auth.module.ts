36b1b2d5144e88ee61fe82482c1a5a08e6390a98
Christian Preß <cpress@it4ipm.de>
2022-03-07 18:09:40 +0100

    add auth guard

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,15 +2,15 @@ import {ModuleWithProviders, NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {LoginComponent} from './login/login.component';
 import {MatCardModule} from '@angular/material/card';
-import { MatInputModule } from '@angular/material/input';
+import {MatInputModule} from '@angular/material/input';
 import {RouterModule} from '@angular/router';
 import {ReactiveFormsModule} from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
-import { StoreModule } from '@ngrx/store';
+import {StoreModule} from '@ngrx/store';
 import {AuthService} from './auth.service';
-import { EffectsModule } from '@ngrx/effects';
 import * as fromAuth from './reducers';
 import {authReducer} from './reducers';
+import {AuthGuard} from './auth.guard';
 
 @NgModule({
     imports: [
@@ -31,7 +31,8 @@ export class AuthModule {
         return {
             ngModule: AuthModule,
             providers: [
-              AuthService
+              AuthService,
+              AuthGuard
             ]
         };
     }
